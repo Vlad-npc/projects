d7f9fdc1f2014d1c0f821f30e358c0d01bea07ae
Vlad-npc <v.buxmann@gmail.com>
2025-01-05 19:59:24 +0100

    diffrence between mut and shadowing

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let mut x = 5; // mutable number, otherwise an error occurs in the compiler
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let x = 5; //unmutable variable
+
+    let x = x + 1; // declaration of a variable with the same name, shadowing is used here
     
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; //declaration of constants
+    {
+        let x = x * 2;
+
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
