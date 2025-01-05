73e47a669e67bc7fd0f6265759b7ed9779c74cf1
Vlad-npc <v.buxmann@gmail.com>
2025-01-05 18:26:16 +0100

    adding a random number generator

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,11 @@
 use std::io; // io library that comes from the standard library. input/output library
-
+use rand::Rng; // from the crate rand with the Rng trait
 fn main() {
     println!("Guess the number!"); // println! macro to print a string to the screen.
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // funktion with a particular random number generator
+    // gen_range gives the range of numbers and is inclusive on the lower and upper bounds
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
