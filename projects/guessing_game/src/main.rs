69ab147d125ae917721ac4b6e8306698d7684a20
Vlad-npc <v.buxmann@gmail.com>
2025-01-05 19:07:44 +0100

    final version of the guessing game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -6,9 +6,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100); // funktion with a particular random number generator
     // gen_range gives the range of numbers and is inclusive on the lower and upper bounds
-    println!("The secret number is: {secret_number}");
 
-    loop {
+    loop { 
         println!("Please input your guess.");
 
         let mut guess = String::new(); // mut = mutable, let apples = 5; is by default immutable 
@@ -18,17 +17,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() { //error handling, ignores non number inputs and goes back to the start of the loop
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) { //cant compare string with integer
             Ordering:: Less => println!("Too small!"),
             Ordering:: Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break; // quits the game after a correct guess
+            }
         }
     }
 }
