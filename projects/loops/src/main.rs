02286cf7072da6f12896b9fca790d1204dfe87f7
Vlad-npc <v.buxmann@gmail.com>
2025-01-09 21:53:11 +0100

    for loops

--- projects/loops/src/main.rs
+++ projects/loops/src/main.rs
@@ -1,21 +1,9 @@
 fn main() {
 
-    let mut count = 0;
-    'counting_up: loop{
-        println!("count = {count}");
-        let mut remaining = 10;
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
 
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-        count += 1;
+    while index < 5 {
+        println!
     }
-    println!{"End count = {count}"};
 }
