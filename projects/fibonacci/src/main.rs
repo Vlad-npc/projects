e6ac0e7c5b20b0df5e2d2ce51d004043f8bf9326
Vlad-npc <v.buxmann@gmail.com>
2025-01-11 23:41:43 +0100

    fibonacci done

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -6,25 +6,33 @@ fn main() {
     let mut prior2 = 1;
 
     println!("Calculating the n-th Fibonacci number. Enter number.");
+    loop {
+        let mut n = String::new();
 
-    let mut n = String::new();
+        io::stdin()
+            .read_line(&mut n)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut n)
-        .expect("Failed to read line");
+        let n : u128 = n.trim().parse().expect("REASON");
 
-    let n : u32 = n.trim().parse().expect("REASON");
-
-    if n < 1 {
-        println!("Invalid input.");
-    } else {
-        let mut current = 0;
-        for _number in 1..n {
-            current = prior1 + prior2;
-            prior1 = prior2;
-            prior2 = current;
+        
+        if n < 1 {
+            println!("Invalid input.");
+            continue;
         }
-
-        println!("The {n}-th Fibonacci number is {current}.");
+        if n == 1 {
+            println!("The {n}-th Fibonacci number is 1.");
+            break;
+        }
+        else {
+            let mut current = 0;
+            for _number in 1..n {
+                current = prior1 + prior2;
+                prior1 = prior2;
+                prior2 = current;
+            }
+            println!("The {n}-th Fibonacci number is {current}.");
+            break;
+        }
     }
 }
